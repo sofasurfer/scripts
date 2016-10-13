260ae4c3b6f42feb5cf167ec2439eba1209b2601
kbohnenblust <kbohnenblust@raptus.com>
2016-10-13 14:09:42 +0200

    Add new script to convert videos

---
+++ movieconverter.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# HTML5 video converter
+# ---------------------------------------------------------------------
+
+SOURCEFOLDER='/Users/kbohnenblust/Downloads/Transmission/completed'
+TARGETFOLDER='/Users/kbohnenblust/Downloads/Transmission/stream'
+
+
+cd $SOURCEFOLDER
+
+# Loop all Folders
+for dic in "$SOURCEFOLDER/*"
+do
+    foldername=$(basename $dic)
+    targetfolder="$TARGETFOLDER/$foldername"
+
+    # Check if fodler exist
+    if [ -d "$targetfolder" ]; then
+        echo "Folder $targetfolder exist."
+        break
+    fi
+    mkdir $targetfolder
+    echo $targetfolder
+
+    # Loop all Files in Fodler
+    for file in $dic/*
+    do 
+        filetype=$(file -b --mime-type $file)
+        filename=$(basename $file)
+
+        # Check Filetype
+        if [ $filetype = 'application/octet-stream' ]; then 
+            ffmpeg -i "$file" -vcodec copy -acodec copy "$targetfolder/${filename%.*}.mp4"
+        fi
+
+        if [ $filetype = 'image/jpeg' ]; then 
+            cp "$file" "$targetfolder/poster.jpg"
+        fi
+        echo " - $filename / $filetype"
+    done
+done
